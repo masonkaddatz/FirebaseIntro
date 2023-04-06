6d1f35b98da1c656a20f58988feeceef78e507ae
masonk123 <mkaddatz@wisc.edu>
2023-04-06 15:47:38 -0500

    initialize

---
+++ app2.js
@@ -0,0 +1,71 @@
+// Task 1
+
+// objects
+let realmadrid = {
+  team_name: "Real Madrid",
+  city: "Madrid",
+  country: "Spain",
+  top_scorers: ["Ronaldo", "Benzema", "Hazard"],
+  worldwide_fans_in_millions: 798,
+};
+
+let barcelona = {
+  team_name: "Barcelona",
+  city: "Barcelona",
+  country: "Spain",
+  top_scorers: ["Messi", "Suarez", "Puyol"],
+  worldwide_fans_in_millions: 738,
+};
+
+let manchesterunited = {
+  team_name: "Manchester United",
+  city: "Manchester",
+  country: "England",
+  top_scorers: ["Cantona", "Rooney", "Ronaldo"],
+  worldwide_fans_in_millions: 755,
+};
+
+let manchestercity = {
+  team_name: "Manchester City",
+  city: "Manchester",
+  country: "England",
+  top_scorers: ["Sterling", "Aguero", "Haaland"],
+  worldwide_fans_in_millions: 537,
+};
+
+let brazilnationalteam = {
+  team_name: "Brazil National Team",
+  city: "na",
+  country: "Brazil",
+  top_scorers: ["Ronaldinho", "Cafu", "Bebeto"],
+  worldwide_fans_in_millions: 950,
+};
+
+let argentinanationalteam = {
+  team_name: "Argentina National Team",
+  city: "na",
+  country: "Argentina",
+  top_scorers: ["Messi", "Batistuta", "Maradona"],
+  worldwide_fans_in_millions: 888,
+};
+
+let atleticomadrid = {
+  team_name: "Atletico Madrid",
+  city: "Madrid",
+  country: "Spain",
+  top_scorers: ["Aragonés", "Griezmann", "Torez"],
+  worldwide_fans_in_millions: 400,
+};
+
+// store in database collection
+// db.collection("teams").add(realmadrid);
+// db.collection("teams").add(barcelona);
+// db.collection("teams").add(manchesterunited);
+// db.collection("teams").add(manchestercity);
+// db.collection("teams").add(brazilnationalteam);
+// db.collection("teams").add(argentinanationalteam);
+// db.collection("teams").add(atleticomadrid);
+
+// Task 2
+
+// Question 1:
